f8e17cc15952bbd879a18309b297e74bb93a1cb6
nmandel <nrmandel@ucdavis.edu>
2014-10-30 11:40:28 -0700

    completes linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,39 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var keyTail = 0;
+  var keyHead = 0;
+  var count = 0;
 
   list.addToTail = function(value){
+    value = makeNode(value);
+
+    list.tail = value;
+    list[keyTail] = value;
+    if (count === 0) {
+      list.head = value;
+      list[keyHead] = value;
+    }
+    keyTail++;
+    count++;
   };
 
   list.removeHead = function(){
+    var removedHead = list[keyHead]['value'];
+    delete list[keyHead];
+    keyHead++;
+    list.head = list[keyHead];
+    count--;
+    return removedHead;
   };
 
   list.contains = function(target){
+    for (var i = keyHead; i < count; i++) {
+      if (list[i].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
