811dc2a5823176724f9d490c6e74cf9040fabb2f
nmandel <nrmandel@ucdavis.edu>
2014-10-31 16:12:05 -0700

    completes tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,46 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  if (this.value === target) {
+    return true;
+  }
+  // if (Array.isArray(this.children)) {
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  // }
+  return false;
 
+    /*var wasFound = false;
+    var helper = function(parent) {
+      if (parent.value === target) {
+        wasFound = true;
+      }
+      if (Array.isArray(parent.children) && !wasFound) {
+        for (var i = 0; i < parent.children.length; i++) {
+          helper(parent.children[i]);
+        }
+      }
+    }
+    helper(this);
+    return wasFound; */
 };
+
+
 
 
 /*
