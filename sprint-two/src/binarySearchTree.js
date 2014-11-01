f0f9c0f97678a80519429fac0e7734030ee30621
nmandel <nrmandel@ucdavis.edu>
2014-11-01 11:26:35 -0700

    completes binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,52 @@
-var makeBinarySearchTree = function(value){
+var BinarySearchTree = function(value){
+  this.value = value;
+  this.right = null;
+  this.left = null;
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+  var newNode = new BinarySearchTree(value);
+  var traverser = function(node){
+    if (node.value > newNode.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        traverser(node.left);
+      }
+    } else if (node.value < newNode.value) {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        traverser(node.right);
+      }
+    }
+  };
+  traverser(this);
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else {
+    if (this.left !== null && this.left.contains(target)) {
+      return true;
+    } else if (this.right !== null && this.right.contains(target)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+  callback(this.value);
+  if (this.left !== null ) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+};
+
 
 
 /*
